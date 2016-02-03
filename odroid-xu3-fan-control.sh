d8a5b6585e91def74b125e3a7f674b66c761422f
Caelan Sayler <caelantsayler@gmail.com>
2016-02-02 20:51:33 -0700

    Now steps up and down fan speed more intelligently

--- odroid-xu3-fan-control.sh
+++ odroid-xu3-fan-control.sh
@@ -19,7 +19,8 @@ fi
 TEMPERATURE_FILE="/sys/devices/10060000.tmu/temp"
 FAN_MODE_FILE="/sys/devices/odroid_fan.$FAN/fan_mode"
 FAN_SPEED_FILE="/sys/devices/odroid_fan.$FAN/pwm_duty"
-TEMP_STEPS=(2  50000  55000  60000  65000  70000  75000  80000)
+TEMP_UP_STEPS=(2  57000  62000  67000  72000  77000  82000  87000)
+TEMP_DOWN_STEPS=(2  50000  55000  60000  65000  70000  75000  80000)
 FAN_STEPS=(2  64     96     128    160    192    224    255)
 
 #make sure after quiting script fan goes to auto control
@@ -35,16 +36,29 @@ function exit_xu3_only_supported {
 }
 
 function write_status {
-  echo -ne "\r[Status] Temp: $(($1/1000))C, Fan Speed: $(($2*100/255))%"
+  echo -ne "\r[Status] Temp: $(($1/1000))C, Fan Speed: $((${FAN_STEPS[$2]}*100/255))%"
   printf "%0.s " {1..10}
 }
 
-function get_speed {
-  # loop through temps
+function write_step {
+  echo ${FAN_STEPS[$1]} > ${FAN_SPEED_FILE}
+}
+
+function get_speed_up {
   for x in 7 6 5 4 3 2 1 0
   do
-    if (( $1 >= ${TEMP_STEPS[${x}]} )); then
-      return x
+    if (( $1 >= ${TEMP_UP_STEPS[${x}]} )); then
+      return $x
+    fi
+  done
+  return 1
+}
+
+function get_speed_down {
+  for x in 7 6 5 4 3 2 1 0
+  do
+    if (( $1 >= ${TEMP_DOWN_STEPS[${x}]} )); then
+      return $x
     fi
   done
   return 1
@@ -58,45 +72,39 @@ elif [ ! -f $FAN_SPEED_FILE ]; then
   exit_xu3_only_supported "no fan speed file"
 fi
 
-
 current_max_temp=`cat ${TEMPERATURE_FILE} | cut -d: -f2 | sort -nr | head -1`
 echo "Fan control started. Current cpu temp: ${current_max_temp}"
 
-echo 0 > ${FAN_MODE_FILE} #to be sure we can manage fan
-prev_step=0
+# init fan 
+get_speed_up current_max_temp
+prev_step=$?
+prev_time=$SECONDS
+echo 0 > ${FAN_MODE_FILE}
+write_step prev_step
+write_status current_max_temp prev_step
 
 while [ true ];
 do
+  sleep 5
 
   current_max_temp=`cat ${TEMPERATURE_FILE} | cut -d: -f2 | sort -nr | head -1`
+  get_speed_up current_max_temp
+  new_step=$?
 
-  new_fan_speed=0
-  if (( ${current_max_temp} >= 75000 )); then
-    new_fan_speed=255
-  elif (( ${current_max_temp} >= 70000 )); then
-    new_fan_speed=200
-  elif (( ${current_max_temp} >= 68000 )); then
-    new_fan_speed=160
-  elif (( ${current_max_temp} >= 66000 )); then
-    new_fan_speed=120
-  elif (( ${current_max_temp} >= 63000 )); then
-    new_fan_speed=100
-  elif (( ${current_max_temp} >= 60000 )); then
-    new_fan_speed=80
-  elif (( ${current_max_temp} >= 57000 )); then
-    new_fan_speed=64
-  elif (( ${current_max_temp} >= 50000 && ${prev_fan_speed} == 64 )); then
-    new_fan_speed=64
-  else
-    new_fan_speed=2
+  if (( $new_step < $prev_step )); then
+    get_speed_down current_max_temp
+    new_step=$?
   fi
 
-  if (( ${prev_fan_speed} != ${new_fan_speed} )); then
-    #echo "event: adjust; speed: ${new_fan_speed}"
-    echo ${new_fan_speed} > ${FAN_SPEED_FILE}
-    prev_fan_speed=${new_fan_speed}
+  if (( $new_step > $prev_step )); then
+    write_step new_step
+    prev_step=$new_step
+    prev_time=$SECONDS
+  elif (( $new_step < $prev_step )) && (( $SECONDS-$prev_time > 20 )); then
+    write_step new_step
+    prev_step=$new_step
+    prev_time=$SECONDS
   fi
 
-  write_status current_max_temp prev_fan_speed
-  sleep 5
+  write_status current_max_temp prev_step
 done
